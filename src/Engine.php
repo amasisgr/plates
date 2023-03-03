1c5cf55e2fe2e47e77150370ad8821d4e016d40e
Alexander Dimitriadis <alex_necromancer@yahoo.gr>
2023-03-03 04:06:02 +0200

    Template Sections externally + Engine syntax fix
    Allow Template Sections to be used externally (eg to call a layout providing "content" without the need to make a Template for the sole purpose of calling the Layout...)

--- src/Engine.php
+++ src/Engine.php
@@ -320,7 +320,7 @@ class Engine
             } unset($layout);
         }
         
-        if(!empty($layouts)) $tpl->layoutsAdd(0, $layouts);
+        if(!empty($layouts)) $tpl->layouts($layouts);
         
         return $tpl->render($tplData);
     }
